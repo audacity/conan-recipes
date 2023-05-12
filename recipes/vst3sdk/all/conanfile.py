7c4ec9c662d6e5662bf6e7fd253b8b73770a7676
Dmitry Vedenko <dmitry@crsib.me>
2023-05-12 18:21:38 +0300

    `vst3sdk` upgraded to 3.7.7 and supports Conan 2

--- recipes/vst3sdk/all/conanfile.py
+++ recipes/vst3sdk/all/conanfile.py
@@ -1,50 +1,106 @@
-from conans import ConanFile, CMake, tools
+from conan import ConanFile
+from conan.tools.cmake import CMake, CMakeToolchain, cmake_layout
+from conan.tools.files import get, export_conandata_patches, apply_conandata_patches, copy
+
 import os
 
 class Vst3SDKConan(ConanFile):
     name = "vst3sdk"
-    version = "3.7.3"
     license = "https://forums.steinberg.net/t/vst-3-sdk-license/201637"
     url = "https://github.com/audacity/conan-recipes"
     description = "VST3 SDK"
     topics = ("vst3")
     settings = "os", "compiler", "build_type", "arch"
-    generators = "cmake"
-    exports_sources=[ 'CMakeLists.txt', "patches/*" ]
+
+    def export_sources(self):
+        export_conandata_patches(self)
+
+    def layout(self):
+        cmake_layout(self, src_folder="src/vst3sdk")
 
     def source(self):
-        tools.get(**self.conan_data["sources"][self.version])
+        get(self,
+            **self.conan_data["sources"][self.version],
+            strip_root=True,
+            destination=os.path.realpath(os.path.join(self.source_folder, '..')))
 
-        extracted_dir = os.path.join('VST_SDK', 'vst3sdk')
-        tools.rename(extracted_dir, 'vst3sdk')
+        apply_conandata_patches(self)
 
-        tools.patch(base_path="vst3sdk", patch_file="patches/disable-validator.diff")
-        tools.patch(base_path="vst3sdk", patch_file="patches/stdatomic-msvc.diff")
+    def generate(self):
+        tc = CMakeToolchain(self)
+        tc.cache_variables["SMTG_ADD_VST3_HOSTING_SAMPLES"] = False
+        tc.cache_variables["SMTG_ADD_VST3_PLUGINS_SAMPLES"] = False
+        tc.cache_variables["SMTG_ADD_VSTGUI"] = False
+        tc.cache_variables["SMTG_CREATE_BUNDLE_FOR_WINDOWS"] = False
+        tc.cache_variables["SMTG_MYPLUGINS_SRC_PATH"] = False
+        tc.cache_variables["SMTG_RUN_VST_VALIDATOR"] = False
+        tc.cache_variables["SMTG_CREATE_PLUGIN_LINK"] = False
+        tc.generate()
+
+    @property
+    def safe_build_type(self):
+        return "Debug" if self.settings.build_type == "Debug" else "Release"
 
     def build(self):
         cmake = CMake(self)
-        cmake.definitions["SMTG_ADD_VST3_HOSTING_SAMPLES"] = "OFF"
-        cmake.definitions["SMTG_ADD_VST3_PLUGINS_SAMPLES"] = "OFF"
-        cmake.definitions["SMTG_ADD_VSTGUI"] = "OFF"
-        cmake.definitions["SMTG_CREATE_BUNDLE_FOR_WINDOWS"] = "OFF"
-        cmake.definitions["SMTG_MYPLUGINS_SRC_PATH"] = ""
-        cmake.definitions["SMTG_RUN_VST_VALIDATOR"] = "OFF"
-
-        #In-source builds are not allowed
-        cmake.configure(build_folder='build')
-        cmake.build()
+        cmake.configure()
+        cmake.build(build_type=self.safe_build_type)
 
     def package(self):
 
-        self.copy("*.h", dst="include/base", src="vst3sdk/base", keep_path=True)
-        self.copy("*.h", dst="include/pluginterfaces", src="vst3sdk/pluginterfaces", keep_path=True)
-        self.copy("*.h", dst="include/public.sdk/source", src="vst3sdk/public.sdk/source", keep_path=True)
+        copy(self,
+             "*.h",
+             src=os.path.join(self.source_folder,  'vst3sdk', 'base'),
+             dst=os.path.join(self.package_folder, 'include', 'base'),
+             keep_path=True)
 
-        self.copy("*.lib", dst="lib", keep_path=False)
-        self.copy("*.dll", dst="bin", keep_path=False)
-        self.copy("*.so", dst="lib", keep_path=False)
-        self.copy("*.dylib", dst="lib", keep_path=False)
-        self.copy("*.a", dst="lib", keep_path=False)
+        copy(self,
+             "*.h",
+             src=os.path.join(self.source_folder,  'vst3sdk', 'pluginterfaces'),
+             dst=os.path.join(self.package_folder, 'include', 'pluginterfaces'),
+             keep_path=True)
+
+        copy(self,
+             "*.h",
+             src=os.path.join(self.source_folder,  'vst3sdk', 'public.sdk', 'source'),
+             dst=os.path.join(self.package_folder, 'include', 'public.sdk', 'source'),
+             keep_path=True)
+
+        copy(self,
+             "*.h",
+             src=os.path.join(self.source_folder,  'vst3sdk', 'public.sdk', 'source'),
+             dst=os.path.join(self.package_folder, 'include', 'public.sdk', 'source'),
+             keep_path=True)
+
+        copy(self,
+             "*.lib",
+             src=self.build_folder,
+             dst=os.path.join(self.package_folder, 'lib'),
+             keep_path=True)
+
+        copy(self,
+             "*.dll",
+             src=self.build_folder,
+             dst=os.path.join(self.package_folder, 'bin'),
+             keep_path=True)
+
+        copy(self,
+             "*.so",
+             src=self.build_folder,
+             dst=os.path.join(self.package_folder, 'lib'),
+             keep_path=True)
+
+        copy(self,
+             "*.dylib",
+             src=self.build_folder,
+             dst=os.path.join(self.package_folder, 'lib'),
+             keep_path=True)
+
+        copy(self,
+             "*.a",
+             src=self.build_folder,
+             dst=os.path.join(self.package_folder, 'lib'),
+             keep_path=True)
 
     def package_info(self):
         self.cpp_info.components["base"].libs.append("base")
