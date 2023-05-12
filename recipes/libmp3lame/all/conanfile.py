e462c7c45f7c5a3d46c7170ce07cae2c076de0c0
Dmitry Vedenko <dmitry@crsib.me>
2023-05-12 18:06:44 +0300

    `libmp3lame` now supports Conan 2

--- recipes/libmp3lame/all/conanfile.py
+++ recipes/libmp3lame/all/conanfile.py
@@ -1,6 +1,14 @@
-from conans import ConanFile, AutoToolsBuildEnvironment, tools
+from conan import ConanFile
+from conan.tools.apple import fix_apple_shared_install_name
+from conan.tools.env import VirtualBuildEnv
+from conan.tools.files import apply_conandata_patches, chdir, copy, export_conandata_patches, get, rename, replace_in_file, rm, rmdir
+from conan.tools.gnu import Autotools, AutotoolsToolchain
+from conan.tools.layout import basic_layout
+from conan.tools.microsoft import is_msvc, NMakeToolchain
 import os
 import shutil
+
+required_conan_version = ">=1.55.0"
 
 
 class LibMP3LameConan(ConanFile):
@@ -8,103 +16,139 @@ class LibMP3LameConan(ConanFile):
     url = "https://github.com/conan-io/conan-center-index"
     description = "LAME is a high quality MPEG Audio Layer III (MP3) encoder licensed under the LGPL."
     homepage = "http://lame.sourceforge.net"
-    topics = ("conan", "libmp3lame", "multimedia", "audio", "mp3", "decoder", "encoding", "decoding")
+    topics = "multimedia", "audio", "mp3", "decoder", "encoding", "decoding"
     license = "LGPL-2.0"
-    exports_sources = ["6410.patch", "6416.patch", "android.patch", "lametime.patch"]
+
     settings = "os", "arch", "compiler", "build_type"
-    options = {"shared": [True, False], "fPIC": [True, False]}
-    default_options = {"shared": False, "fPIC": True}
-    _autotools = None
+    options = {
+        "shared": [True, False],
+        "fPIC": [True, False],
+    }
+    default_options = {
+        "shared": False,
+        "fPIC": True,
+    }
 
     @property
-    def _use_winbash(self):
-        return tools.os_info.is_windows and (self.settings.compiler == "gcc" or tools.cross_building(self.settings))
+    def _is_clang_cl(self):
+        return str(self.settings.compiler) in ["clang"] and str(self.settings.os) in ['Windows']
 
     @property
-    def _is_msvc(self):
-        return str(self.settings.compiler) in ["Visual Studio", "msvc"]
+    def _settings_build(self):
+        return getattr(self, "settings_build", self.settings)
 
-    @property
-    def _source_subfolder(self):
-        return "source_subfolder"
+    def export_sources(self):
+        export_conandata_patches(self)
 
     def config_options(self):
-        if self.settings.os == 'Windows':
+        if self.settings.os == "Windows":
             del self.options.fPIC
 
-    def source(self):
-        tools.get(**self.conan_data["sources"][self.version])
-        extracted_dir = "lame-" + self.version
-        tools.rename(extracted_dir, self._source_subfolder)
+    def configure(self):
+        if self.options.shared:
+            self.options.rm_safe("fPIC")
+        self.settings.rm_safe("compiler.cppstd")
+        self.settings.rm_safe("compiler.libcxx")
 
-    def _apply_patch(self):
-        tools.replace_in_file(os.path.join(self._source_subfolder, 'include', 'libmp3lame.sym'), 'lame_init_old\n', '')
-        for patch in ['6410', '6416', 'lametime']:
-            tools.patch(base_path=self._source_subfolder, patch_file='%s.patch' % patch, strip=3)
-        tools.patch(base_path=self._source_subfolder, patch_file='android.patch')
+    def layout(self):
+        basic_layout(self, src_folder="src")
+        self.folders.build = 'build'
+        self.folders.generators = 'build/conan'
+
+    def build_requirements(self):
+        if not is_msvc(self) and not self._is_clang_cl:
+            self.tool_requires("gnu-config/cci.20210814@audacity/stable")
+            if self._settings_build.os == "Windows":
+                self.win_bash = True
+                if not self.conf.get("tools.microsoft.bash:path", check_type=str):
+                    self.tool_requires("msys2/cci.latest@audacity/stable")
+
+    def source(self):
+        get(self, **self.conan_data["sources"][self.version], strip_root=True)
+
+    def generate(self):
+        if is_msvc(self) or self._is_clang_cl:
+            tc = NMakeToolchain(self)
+            tc.generate()
+        else:
+            env = VirtualBuildEnv(self)
+            env.generate()
+            tc = AutotoolsToolchain(self)
+            tc.configure_args.append("--disable-frontend")
+            if self.settings.compiler == "clang" and self.settings.arch in ["x86", "x86_64"]:
+                tc.extra_cxxflags.extend(["-mmmx", "-msse"])
+            tc.generate()
 
     def _build_vs(self):
-        with tools.chdir(self._source_subfolder):
-            shutil.copy('configMS.h', 'config.h')
-            command = 'nmake -f Makefile.MSVC comp=msvc asm=yes'
-            if self.settings.arch == 'x86_64':
-                tools.replace_in_file('Makefile.MSVC', 'MACHINE = /machine:I386', 'MACHINE =/machine:X64')
-                command += ' MSVCVER=Win64'
-            if self.options.shared:
-                command += ' dll'
-            with tools.vcvars(self.settings, filter_known_paths=False, force=True):
-                self.run(command)
-
-    def _configure_autotools(self):
-        if not self._autotools:
-            args = []
-            if self.options.shared:
-                args.extend(['--disable-static', '-enable-shared'])
+        with chdir(self, self.source_folder):
+            shutil.copy2("configMS.h", "config.h")
+            # Honor vc runtime
+            replace_in_file(self, "Makefile.MSVC", "CC_OPTS = $(CC_OPTS) /MT", "")
+            # Do not hardcode LTO
+            replace_in_file(self, "Makefile.MSVC", " /GL", "")
+            replace_in_file(self, "Makefile.MSVC", " /LTCG", "")
+            replace_in_file(self, "Makefile.MSVC", "ADDL_OBJ = bufferoverflowU.lib", "")
+            command = "nmake -f Makefile.MSVC comp=msvc"
+            if self._is_clang_cl:
+                compilers_from_conf = self.conf.get("tools.build:compiler_executables", default={}, check_type=dict)
+                buildenv_vars = VirtualBuildEnv(self).vars()
+                cl = compilers_from_conf.get("c", buildenv_vars.get("CC", "clang-cl"))
+                link = buildenv_vars.get("LD", "lld-link")
+                replace_in_file(self, "Makefile.MSVC", "CC = cl", f"CC = {cl}")
+                replace_in_file(self, "Makefile.MSVC", "LN = link", f"LN = {link}")
+                # what is /GAy? MSDN doesn't know it
+                # clang-cl: error: no such file or directory: '/GAy'
+                # https://docs.microsoft.com/en-us/cpp/build/reference/ga-optimize-for-windows-application?view=msvc-170
+                replace_in_file(self, "Makefile.MSVC", "/GAy", "/GA")
+            if self.settings.arch == "x86_64":
+                replace_in_file(self, "Makefile.MSVC", "MACHINE = /machine:I386", "MACHINE =/machine:X64")
+                command += " MSVCVER=Win64 asm=yes"
+            elif self.settings.arch == "armv8":
+                replace_in_file(self, "Makefile.MSVC", "MACHINE = /machine:I386", "MACHINE =/machine:ARM64")
+                command += " MSVCVER=Win64"
             else:
-                args.extend(['--disable-shared', '--enable-static'])
-            if self.settings.build_type == 'Debug':
-                args.append('--enable-debug')
-            if self.settings.os != 'Windows' and self.options.fPIC:
-                args.append('--with-pic')
+                command += " asm=yes"
+            command += " libmp3lame.dll" if self.options.shared else " libmp3lame-static.lib"
+            self.run(command)
 
-            self._autotools = AutoToolsBuildEnvironment(self, win_bash=self._use_winbash)
-            if self.settings.compiler == 'clang' and self.settings.arch in ['x86', 'x86_64']:
-                self._autotools.flags.extend(['-mmmx', '-msse'])
-            self._autotools.configure(args=args, configure_dir=self._source_subfolder)
-        return self._autotools
-
-    def _build_configure(self):
-        autotools = self._configure_autotools()
+    def _build_autotools(self):
+        for gnu_config in [
+            self.conf.get("user.gnu-config:config_guess", check_type=str),
+            self.conf.get("user.gnu-config:config_sub", check_type=str),
+        ]:
+            if gnu_config:
+                copy(self, os.path.basename(gnu_config), src=os.path.dirname(gnu_config), dst=self.source_folder)
+        autotools = Autotools(self)
+        autotools.configure()
         autotools.make()
 
     def build(self):
-        self._apply_patch()
-        if self._is_msvc:
+        apply_conandata_patches(self)
+        replace_in_file(self, os.path.join(self.source_folder, "include", "libmp3lame.sym"), "lame_init_old\n", "")
+
+        if is_msvc(self) or self._is_clang_cl:
             self._build_vs()
         else:
-            self._build_configure()
+            self._build_autotools()
 
     def package(self):
-        self.copy(pattern="LICENSE", src=self._source_subfolder, dst="licenses")
-        if self._is_msvc:
-            self.copy(pattern='*.h', src=os.path.join(self._source_subfolder, 'include'), dst=os.path.join('include', 'lame'))
-            self.copy(pattern='*.lib', src=os.path.join(self._source_subfolder, 'output'), dst='lib')
-            self.copy(pattern='*.exe', src=os.path.join(self._source_subfolder, 'output'), dst='bin')
+        copy(self, pattern="LICENSE", src=self.source_folder, dst=os.path.join(self.package_folder,"licenses"))
+        if is_msvc(self) or self._is_clang_cl:
+            copy(self, pattern="*.h", src=os.path.join(self.source_folder, "include"), dst=os.path.join(self.package_folder,"include", "lame"))
+            name = "libmp3lame.lib" if self.options.shared else "libmp3lame-static.lib"
+            copy(self, name, src=os.path.join(self.source_folder, "output"), dst=os.path.join(self.package_folder,"lib"))
             if self.options.shared:
-                self.copy(pattern='*.dll', src=os.path.join(self._source_subfolder, 'output'), dst='bin')
-            name = 'libmp3lame.lib' if self.options.shared else 'libmp3lame-static.lib'
-            shutil.move(os.path.join(self.package_folder, 'lib', name),
-                        os.path.join(self.package_folder, 'lib', 'mp3lame.lib'))
+                copy(self, pattern="*.dll", src=os.path.join(self.source_folder, "output"), dst=os.path.join(self.package_folder,"bin"))
+            rename(self, os.path.join(self.package_folder, "lib", name),
+                         os.path.join(self.package_folder, "lib", "mp3lame.lib"))
         else:
-            autotools = self._configure_autotools()
+            autotools = Autotools(self)
             autotools.install()
-            tools.rmdir(os.path.join(self.package_folder, "bin"))
-        tools.rmdir(os.path.join(self.package_folder, 'share'))
-        la_file = os.path.join(self.package_folder, "lib", "libmp3lame.la")
-        if os.path.isfile(la_file):
-            os.unlink(la_file)
+            rmdir(self, os.path.join(self.package_folder, "share"))
+            rm(self, "*.la", os.path.join(self.package_folder, "lib"))
+            fix_apple_shared_install_name(self)
 
     def package_info(self):
         self.cpp_info.libs = ["mp3lame"]
-        if self.settings.os == "Linux":
+        if self.settings.os in ["Linux", "FreeBSD"]:
             self.cpp_info.system_libs = ["m"]
