0bb8dc990766bfe7c4797b3e4cf9d26cc25cd8bc
Dmitry Vedenko <dmitry@crsib.me>
2023-05-19 18:16:42 +0300

    Fixes Windows crossbuild of libcurl

--- recipes/libcurl/all/test_package/conanfile.py
+++ recipes/libcurl/all/test_package/conanfile.py
@@ -29,24 +29,3 @@ class TestPackageConan(ConanFile):
     def test(self):
         if can_run(self):
             self.run(self._test_executable, env="conanrun")
-        else:
-            # We will dump information for the generated executable
-            if self.settings.os in ["Android", "iOS"]:
-                # FIXME: Check output for these hosts
-                return
-
-            output = subprocess.check_output(["file", self._test_executable]).decode()
-
-            if self.settings.os == "Macos" and self.settings.arch == "armv8":
-                assert "Mach-O 64-bit executable arm64" in output, f"Not found in output: {output}"
-
-            elif self.settings.os == "Linux":
-                if self.settings.arch == "armv8_32":
-                    assert re.search(r"Machine:\s+ARM", output), f"Not found in output: {output}"
-                elif "armv8" in self.settings.arch:
-                    assert re.search(r"Machine:\s+AArch64", output), f"Not found in output: {output}"
-                elif "arm" in self.settings.arch:
-                    assert re.search(r"Machine:\s+ARM", output), f"Not found in output: {output}"
-
-            elif self.settings.os == "Windows": # FIXME: It satisfies not only MinGW
-                assert re.search(r"PE32.*executable.*Windows", output), f"Not found in output: {output}"
