d38093179c0f2435205f83018aff764d8c8b9a43
Dmitry Vedenko <dmitry@crsib.me>
2023-07-05 14:04:37 +0300

    `kddockwidgets` now supports Conan 2 (Windows tested)

--- recipes/kddockwidgets/all/conanfile.py
+++ recipes/kddockwidgets/all/conanfile.py
@@ -7,13 +7,14 @@
 # Contact KDAB at <info@kdab.com> for commercial licensing options.
 #
 
-from conans import ConanFile, tools
-from conan.tools.cmake import cmake_layout, CMakeDeps, CMake, CMakeToolchain
+from conan import ConanFile
+from conan.tools.files import get
 from conan.tools.build import cross_building
+from conan.tools.cmake import CMake, CMakeToolchain, CMakeDeps, cmake_layout
+from conan.tools.scm import Version
 
 class KDDockWidgetsConan(ConanFile):
     name = "kddockwidgets"
-    version = "1.6.0"
     default_user = "audacity"
     default_channel = "testing"
     license = ("https://raw.githubusercontent.com/KDAB/KDDockWidgets/master/LICENSES/GPL-2.0-only.txt",
@@ -21,11 +22,10 @@ class KDDockWidgetsConan(ConanFile):
     author = "Klaralvdalens Datakonsult AB (KDAB) info@kdab.com"
     url = "https://github.com/KDAB/KDDockWidgets"
     description = "Advanced Dock Widget Framework for Qt"
-    generators = "CMakeDeps"
     topics = ("qt", "dockwidget", "kdab")
     settings = "os", "compiler", "build_type", "arch"
     options = {
-        "qt_version": "ANY",
+        "qt_version": ["ANY"],
         "shared": [True, False],
         "build_examples": [True, False],
         "build_tests": [True, False],
@@ -34,7 +34,7 @@ class KDDockWidgetsConan(ConanFile):
     }
 
     default_options = {
-        "qt_version": "qt/6.3.1@audacity/testing",
+        "qt_version": "qt/6.3.1@audacity/stable",
         "shared": False,
         "build_examples": False,
         "build_tests": False,
@@ -42,60 +42,72 @@ class KDDockWidgetsConan(ConanFile):
         "build_for_qt6": True,
     }
 
+    @property
+    def __use_system_qt(self):
+        return str(self.options.qt_version) == 'system'
+
     def requirements(self):
-        # Check https://docs.conan.io/en/latest/reference/conanfile/attributes.html#version-ranges for more info about versioning
-        self.requires(str(self.options.qt_version))
+        if not self.__use_system_qt:
+            # Check https://docs.conan.io/en/latest/reference/conanfile/attributes.html#version-ranges for more info about versioning
+            self.requires(str(self.options.qt_version))
 
     def build_requirements(self):
-        self.tool_requires("ninja/1.11.0")
+        self.tool_requires("ninja/1.11.0@audacity/stable")
 
         if cross_building(self, skip_x64_x86=True):
-            self.tool_requires("qt-tools/6.3.1@audacity/testing")
+            self.tool_requires("qt-tools/6.3.1@audacity/stable", package_id_mode="minor_mode")
+
+    def layout(self):
+        cmake_layout(self, src_folder="src")
 
     def source(self):
-        tools.get(**self.conan_data["sources"][self.version])
-        extracted_dir = self.name + "-" + self.version
-        tools.rename(extracted_dir, "source_subfolder")
+        get(self, **self.conan_data["sources"][self.version],
+            destination=self.source_folder, strip_root=True)
 
     def configure(self):
         if self.options.shared:
             self.options["*"].shared = True
-
-        self.options["qt"].qtdeclarative = True
-        # Required to enable Quick
-        self.options["qt"].qtshadertools = True
+        if not self.__use_system_qt:
+            self.options["qt"].qtdeclarative = True
+            self.options["qt"].qtshadertools = True
 
     def generate(self):
+        CMakeDeps(self).generate()
+
         tc = CMakeToolchain(self, generator="Ninja")
         tc.cache_variables["KDDockWidgets_STATIC"] = not self.options.shared
         tc.cache_variables["KDDockWidgets_EXAMPLES"] = self.options.build_examples
         tc.cache_variables["KDDockWidgets_TESTS"] = self.options.build_tests
         tc.cache_variables["KDDockWidgets_PYTHON_BINDINGS"] = self.options.build_python_bindings
         tc.cache_variables["KDDockWidgets_QT6"] = self.options.build_for_qt6
-        tc.cache_variables["KDDockWidgets_QTQUICK"] = True
-        if cross_building(self, skip_x64_x86=True):
+
+        if Version(self.version) < "2.0.0":
+            tc.cache_variables["KDDockWidgets_QTQUICK"] = True
+        else:
+            tc.cache_variables["KDDockWidgets_FRONTENDS"] = 'qtwidgets;qtquick'
+
+        if not self.__use_system_qt and cross_building(self, skip_x64_x86=True):
             host_tools = self.dependencies.direct_build["qt-tools"].package_folder
             tc.cache_variables["QT_HOST_PATH"] = host_tools
         tc.generate()
 
-        deps = CMakeDeps(self)
-        deps.generate()
-
-    def layout(self):
-        cmake_layout(self)
-
     def build(self):
-        self.cmake = CMake(self)
-        self.cmake.configure(build_script_folder="source_subfolder")
-        self.cmake.build()
+        cmake = CMake(self)
+        cmake.configure()
+        cmake.build()
 
     def package(self):
-        self.cmake.install()
+        cmake = CMake(self)
+        cmake.install()
 
     def package_info(self):
         self.env_info.CMAKE_PREFIX_PATH.append(self.package_folder)
-        self.cpp_info.includedirs = [ "include/kddockwidgets-qt6" ]
 
-    def package_id(self):
-        # Check only the major and minor version!
-        self.info.requires["qt"].minor_mode()
+        self.cpp_info.set_property("cmake_find_mode", "config")
+        self.cpp_info.set_property("cmake_target_name", "KDAB::kddockwidgets")
+
+        if self.options.build_for_qt6:
+            self.cpp_info.includedirs = [ "include/kddockwidgets-qt6" ]
+            self.cpp_info.set_property("cmake_file_name", "KDDockWidgets-qt6")
+        else:
+            self.cpp_info.set_property("cmake_file_name", "KDDockWidgets")
