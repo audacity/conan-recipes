9868261c8a5ad5b43e12829e5fc62607c4f9da7b
Dmitry Vedenko <dmitry@crsib.me>
2023-10-03 20:53:02 +0300

    meson updated to 1.2.2

--- recipes/harfbuzz/all/conanfile.py
+++ recipes/harfbuzz/all/conanfile.py
@@ -142,7 +142,7 @@ class HarfbuzzConan(ConanFile):
                   destination=self.source_folder, strip_root=True)
 
     def build_requirements(self):
-        self.tool_requires("meson/1.0.1@audacity/stable")
+        self.tool_requires("meson/1.2.2@audacity/stable")
         if not self.conf.get("tools.gnu:pkg_config", default=False, check_type=str):
             self.tool_requires("pkgconf/1.9.3@audacity/stable")
 
