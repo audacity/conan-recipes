e1bd1df53376b8cd9407fd773816e441cd4fba92
Dmitry Vedenko <dmitry@crsib.me>
2024-03-06 22:57:40 +0300

    [WavPack] Move patch application to the build folder

--- recipes/wavpack/all/conanfile.py
+++ recipes/wavpack/all/conanfile.py
@@ -38,7 +38,6 @@ class WavPackConan(ConanFile):
 
     def source(self):
         get(self, **self.conan_data["sources"][self.version], strip_root=True)
-        apply_conandata_patches(self)
 
     def generate(self):
         tc = CMakeToolchain(self)
@@ -58,6 +57,7 @@ class WavPackConan(ConanFile):
 
 
     def build(self):
+        apply_conandata_patches(self)
         cmake = CMake(self)
         cmake.configure()
         cmake.build()
